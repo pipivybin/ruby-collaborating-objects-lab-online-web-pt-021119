542f03af7c27749fdc71bf4eb8fa90a3bfc1d124
Ming Duan <11203duan@gmail.com>
2019-03-24 18:08:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ attr_accessor :name, :artist
 
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
-    song.artist = filename.split(" - ")[0]
+    song.artist_name = filename.split(" - ")[0]
     song
   end
 
